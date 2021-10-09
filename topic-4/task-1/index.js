43e5f1959d7371df77c69344aa04dfc669d1e367
onezuppi <52073821+onezuppi@users.noreply.github.com>
2021-10-10 00:02:08 +0500

    Completed all tasks

--- topic-4/task-1/index.js
+++ topic-4/task-1/index.js
@@ -39,19 +39,115 @@ const tokens = {
     }
 }
 
+const budget = 5000;
+
 
 /**
  * 
  * @param {*} token токен
  */
-function Coin(token) { }
+function Coin(token) {
+    if (!(token in tokens)){
+        throw new Error("Argument error. Token is incorrect.");
+    }
+    this.price = this.parseParameter(tokens[token].price);
+    this.priceChange24h = this.parseParameter(tokens[token].priceChange24h);
+    this.name = token;
+}
+
+Coin.prototype.parseParameter = function (parameter){
+    return Number.parseFloat(parameter.substring(0, parameter.length - 1));
+}
+
+Coin.prototype.GetProfitWithOnlyIncreasing = function (daysCount, startPrice) {
+    startPrice = startPrice || this.price;
+    const finalPrice = startPrice * (1 + (this.priceChange24h / 100)) ** daysCount;
+
+    return {"profit": finalPrice - startPrice, finalPrice};
+}
+
+Coin.prototype.GetProfitWithOnlyDecreasing = function (daysCount, startPrice) {
+    startPrice = startPrice || this.price;
+    const finalPrice = startPrice * (1 - (this.priceChange24h / 100)) ** daysCount;
+
+    return {"profit": finalPrice - startPrice, finalPrice};
+}
+
+Coin.prototype.GetProfitWithHalfTrend = function (daysCount, firstTrend, secondTrend) {
+    const daysCountHalf = Math.floor(daysCount / 2);
+    const firstTrendProfit = firstTrend(daysCountHalf);
+    const secondTrendProfit = secondTrend(daysCountHalf + (daysCount % 2), firstTrendProfit["finalPrice"]);
+
+    return firstTrendProfit["profit"] + secondTrendProfit["profit"];
+}
+
+Coin.prototype.GetProfitWithIncreasingAndDecreasing = function (daysCount) {
+    return this.GetProfitWithHalfTrend(daysCount, this.GetProfitWithOnlyIncreasing, this.GetProfitWithOnlyDecreasing);
+}
+
+Coin.prototype.GetProfitWithDecreasingAndIncreasing = function (daysCount) {
+    return this.GetProfitWithHalfTrend(daysCount, this.GetProfitWithOnlyDecreasing, this.GetProfitWithOnlyIncreasing);
+}
+
+Coin.prototype.GetProfitWithDayCondition = function (daysCount, DayCondition) {
+    let [profit, finalPrice] = [0, this.price];
+    for (let day = 0; day < daysCount; day++){
+        let currentProfit = 0;
+        if (DayCondition(day)){
+            [currentProfit, finalPrice] = this.GetProfitWithOnlyIncreasing(1, finalPrice);
+        } else {
+            [currentProfit, finalPrice] = this.GetProfitWithOnlyDecreasing(1, finalPrice);
+        }
+        profit += currentProfit;
+    }
+
+    return profit;
+}
+
+Coin.prototype.GetProfitWithAlternation = function (daysCount) {
+    return this.GetProfitWithDayCondition(daysCount, day => day % 2 === 0);
+}
+
+Coin.prototype.GetProfitWithRandom = function (daysCount) {
+    return this.GetProfitWithDayCondition(daysCount, () => Math.random() > 0.5);
+}
+
+
+Coin.prototype.calculateAverageProfit = function (daysCount){
+    const profits = [this.GetProfitWithOnlyIncreasing(daysCount), this.GetProfitWithOnlyDecreasing(daysCount),
+        this.GetProfitWithDecreasingAndIncreasing(daysCount), this.GetProfitWithIncreasingAndDecreasing(daysCount),
+        this.GetProfitWithAlternation(daysCount), this.GetProfitWithRandom(daysCount)];
+
+    return profits.reduce((sum, profit) => sum + profit) / profits.length;
+}
+
 
 /**
  * 
  * @param {*} months массив месяцев, формат {month, year}
  * @return название токена
  */
-function tokenChoice(months) { }
+function tokenChoice(months) {
+    if (months.some(date => Object.values(date).some(x => !Number.isInteger(x)) || date["month"] > 12 || date["month"] <= 0)){
+        throw new Error("Argument error. Months format is incorrect.");
+    }
+
+    const daysCount = months.reduce((count, date) => count + (new Date(date['year'], date['month'], 0)).getDate());
+    const coins = Object.keys(tokens).map(token => new Coin(token));
+
+    let maxProfit = 0;
+    let maxProfitCoin = coins[0];
+    for (const coin in coins) {
+        if (coin.price < budget){
+            const profit = coin.calculateAverageProfit(daysCount);
+            if (profit > maxProfit) {
+                [maxProfit, maxProfitCoin] = [profit, coin];
+            }
+        }
+    }
+
+    return maxProfitCoin.name;
+}
 
 
 module.exports.tokenChoice = tokenChoice;
--- topic-4/task-2/index.js
+++ topic-4/task-2/index.js
@@ -7,7 +7,7 @@
 * салатом (+ 20 тугриков, + 5 калорий)
 * картофелем (+ 15 тугриков, + 10 калорий)
 * Дополнительно, гамбургер можно посыпать приправой (+ 15 тугриков, 0 калорий) и полить майонезом (+ 20 тугриков, + 5 калорий).
-* Напиши программу, расчиытвающую стоимость и калорийность гамбургера. Используй ООП подход (подсказка: нужен класс Гамбургер, 
+* Напиши программу, насчитывающую стоимость и калорийность гамбургера. Используй ООП подход (подсказка: нужен класс Гамбургер,
 * константы, методы для выбора опций и рассчета нужных величин).
 * Код должен быть защищен от ошибок. Представь, что твоим классом будет пользоваться другой программист. 
 * Если он передает неправильный тип гамбургера, например, или неправильный вид добавки, должно выбрасываться исключение 
@@ -32,46 +32,96 @@ Hamburger.STUFFING_POTATO = 'STUFFING_POTATO' //price 15 kal 10
 Hamburger.TOPPING_MAYO = 'TOPPING_MAYO'; //price 20 kal 5
 Hamburger.TOPPING_SPICE = 'TOPPING_SPICE'; //price 15
 
-function Hamburger(size, stuffing) {
- 
+Hamburger.SIZES = [Hamburger.SIZE_SMALL, Hamburger.SIZE_LARGE];
+Hamburger.STUFFING = [Hamburger.STUFFING_CHEESE, Hamburger.STUFFING_SALAD, Hamburger.STUFFING_POTATO];
+Hamburger.TOPPINGS = [Hamburger.TOPPING_SPICE, Hamburger.TOPPING_MAYO];
+
+Hamburger.PRICES = {[Hamburger.SIZE_SMALL]: 50, [Hamburger.SIZE_LARGE]: 100,
+    [Hamburger.STUFFING_CHEESE]: 10, [Hamburger.STUFFING_SALAD]: 20,
+    [Hamburger.STUFFING_POTATO]: 15, [Hamburger.TOPPING_MAYO]: 20,
+    [Hamburger.TOPPING_SPICE]: 15};
+
+Hamburger.CALORIES = {[Hamburger.SIZE_SMALL]: 20, [Hamburger.SIZE_LARGE]: 40,
+    [Hamburger.STUFFING_CHEESE]: 20, [Hamburger.STUFFING_SALAD]: 5,
+    [Hamburger.STUFFING_POTATO]: 10, [Hamburger.TOPPING_MAYO]: 5,
+    [Hamburger.TOPPING_SPICE]: 0};
+
+
+function Hamburger(size, ...stuffing) {
+    if (Hamburger.SIZES.indexOf(size) === -1){
+        throw new Error("HamburgerException. Size is incorrect.");
+    }
+    if (stuffing.some(stuff => Hamburger.STUFFING.indexOf(stuff) === -1)){
+        throw new Error("HamburgerException. Stuffing is incorrect.");
+    }
+    
+    this.size = size; 
+    this.stuffing = stuffing;
+    this.toppings = []
 }
+
  
 /*Добавить добавку к гамбургеру. Можно добавить несколько
 * добавок, при условии, что они разные.
 * @param topping     Тип добавки
 * @throws {HamburgerException}  При неправильном использовании*/
 Hamburger.prototype.addTopping = function (topping) {
+    if (Hamburger.TOPPINGS.indexOf(topping) === -1){
+        throw new Error("HamburgerException. Topping is incorrect.");
+    }
+
+    this.toppings.push(topping);
 }
  
 /* Убрать добавку, при условии, что она ранее была
  * добавлена.
  * @param topping   Тип добавки
  * @throws {HamburgerException}  При неправильном использовании*/
- Hamburger.prototype.removeTopping = function (topping) {
- }
+Hamburger.prototype.removeTopping = function (topping) {
+    if (Hamburger.TOPPINGS.indexOf(topping) === -1){
+        throw new Error("HamburgerException. Topping is incorrect.");
+    }
+
+    this.toppings.splice(topping.indexOf(topping), 1);
+}
  
 /* Получить список добавок.
  * @return {Array} Массив добавленных добавок, содержит константы
  *                 Hamburger.TOPPING_**/
- Hamburger.prototype.getToppings = function () {
- }
+Hamburger.prototype.getToppings = function () {
+    return this.toppings;
+}
  
 /* Узнать размер гамбургера */
-Hamburger.prototype.getSize = function (size) {  }
+Hamburger.prototype.getSize = function (size) {
+    return this.size;
+}
  
 /* Узнать начинку гамбургера */
 Hamburger.prototype.getStuffing = function () {
+    return this.stuffing;
 }
- 
+
+Hamburger.prototype.calculate = function (valuesProvider) {
+    const stuffingValues = this.stuffing.map(stuff => valuesProvider[stuff]);
+    const stuffingValuesSum = stuffingValues.reduce((cost, stuffPrice) => cost + stuffPrice);
+    const toppingValues = this.toppings.map(topping => valuesProvider[topping]);
+    const toppingValuesSum = toppingValues.reduce((cost, toppingPrice) => cost + toppingPrice);
+
+    return valuesProvider[this.size] + stuffingValuesSum + toppingValuesSum;
+}
+
 /* Узнать цену гамбургера
  * @return {Number} Цена в тугриках */
 Hamburger.prototype.calculatePrice = function () {
+    return this.calculate(Hamburger.PRICES);
 }
  
  
 /* Узнать калорийность
  * @return {Number} Калорийность в калориях */
 Hamburger.prototype.calculateCalories = function () {
+    return this.calculate(Hamburger.CALORIES);
 }
 
 module.exports.Hamburger = Hamburger;
