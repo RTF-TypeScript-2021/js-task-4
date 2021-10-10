852198e73638077963c20137f44fc110cbaea170
makar <misha.torno@gmail.com>
2021-10-10 16:06:25 +0500

    решил первую задачу.не понимаю, что от меня хотели.....

--- topic-4/task-1/index.js
+++ topic-4/task-1/index.js
@@ -3,22 +3,21 @@
  * с надеждой вложить свои деньги в другие токены чтобы отбить потери.
  * По тактике Васи, вкладывать стоит только в токены, которые еще не показали свой максимум
  * Поэтому он выписал себе несколько перспективных, осталось только понять, куда вложиться
- * 
+ *
  * На практике, чтобы определить, куда инвестировать, нужно учитывать большое количество параметров,
  * но Вася не инвестор, поэтому во всей этой тенденции ему важны три параметра:
- * 	- изменение цены за 24 часа
+ *    - изменение цены за 24 часа
  *  - цена
- * 
+ *
  * Бюджет Васи = 5.000$ и он хочет держать на них токены, а потом продать.
- * 
+ *
  * Вася выяснил, на сколько приблизительно меняется цена у каждого токена и записал это в поле priceChange24h
  * Может как уменьшиться, так и увеличиться.
- * 
+ *
  * У вас уже создан базовый класс Coin, вам необходимо создать от него остальные токены
- * 
+ *
  * В итоге по предоставленным данным вы должны выбрать один токен, который принесет максимальную прибыль
-*/
-
+ */
 
 const tokens = {
     ETH: {
@@ -39,19 +38,65 @@ const tokens = {
     }
 }
 
-
 /**
- * 
+ *
  * @param {*} token токен
  */
-function Coin(token) { }
+function Coin(token) {
+    this.price = parseFloat(tokens[token].price);
+    this.priceChange24h = parseFloat(tokens[token].priceChange24h);
+}
+
+Coin.prototype.getMaxPrice = function (days) {
+    return [...Array(days)].reduce((res, _) => res * (1 + this.priceChange24h / 100), this.price);
+}
+
+Coin.prototype.getMinPrice = function (days) {
+    return [...Array(days)].reduce((res, _) => res * (1 - this.priceChange24h / 100), this.price);
+}
+
+Coin.prototype.getAveragePrice = function (days) {
+    return this.getMaxPrice(Math.floor(days / 2)) + this.getMinPrice(Math.ceil(days / 2));
+}
+
+Coin.prototype.calculateStatistic = function (...params) {
+    return params.reduce((x, y) => x + y) / params.length;
+}
+
+function getDaysCount(months) {
+    const startDate = new Date(months[0].year, months[0].month - 1);
+    const endDate = new Date(months[months.length - 1].year, months[months.length - 1].month);
+
+    return Math.floor(endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
+}
 
 /**
- * 
+ *
  * @param {*} months массив месяцев, формат {month, year}
  * @return название токена
  */
-function tokenChoice(months) { }
+
+function tokenChoice(months) {
+    let bestStat = 0;
+    let bestTokenName = "";
+    const daysCount = getDaysCount(months);
+
+    for (const token in tokens) {
+        const coin = new Coin(token);
+        const curStatistic = coin.calculateStatistic(
+            coin.getMinPrice(daysCount),
+            coin.getMaxPrice(daysCount),
+            coin.getAveragePrice(daysCount)
+        );
+
+        if (curStatistic > bestStat) {
+            bestStat = curStatistic;
+            bestTokenName = token;
+        }
+    }
+
+    return bestTokenName;
+}
 
 
 module.exports.tokenChoice = tokenChoice;
--- topic-4/task-1/specs/task-1.spec.ts
+++ topic-4/task-1/specs/task-1.spec.ts
@@ -1,5 +1,9 @@
-const { tokenChoice } = require('../index');
+const {tokenChoice} = require('../index');
 
 test('Method works fine', () => {
-    expect(tokenChoice([{ month: 11, year: 2022 }])).toEqual("ETH");
+    expect(tokenChoice([{month: 11, year: 2022}])).toEqual("ETH");
+});
+
+test('Method works fine', () => {
+    expect(tokenChoice([{month: 11, year: 2022}, {month: 1, year: 2023}, {month: 2, year: 2023}])).toEqual("ETH");
 });
