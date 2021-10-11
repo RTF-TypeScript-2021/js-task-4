38b5381ce4726847727baf9c218971b91fcf0d33
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2021-10-11 19:49:57 +0500

    refactoring

--- topic-4/task-3/index.js
+++ topic-4/task-3/index.js
@@ -7,8 +7,8 @@ const hamsterTypes = {
 
 class Hamster {
     constructor(type) {
-        this.stomach = []
-        this.type = type
+        this.stomach = [];
+        this.type = type;
     }
 
     eat(food) {
@@ -21,15 +21,8 @@ function getSpeedy() {
 }
 
 function getLazy() {
-    return new Hamster(hamsterTypes.lazy)
+    return new Hamster(hamsterTypes.lazy);
 }
-
-// Этот хомяк нашёл еду
-getSpeedy().eat("apple");
-console.log(getSpeedy().stomach); // apple
-
-// У этого хомяка тоже есть еда. Почему? Исправьте
-console.log(getLazy().stomach); // apple
 
 module.exports.getSpeedy = getSpeedy;
 module.exports.getLazy = getLazy;
