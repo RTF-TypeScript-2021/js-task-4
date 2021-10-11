decd286de4af0e15639c992c291a3eb5c1ad6060
Sychqh <sychqh@gmail.com>
2021-10-11 17:14:15 +0500

    Решил 4 задание

--- topic-4/task-4/index.js
+++ topic-4/task-4/index.js
@@ -1,41 +1,35 @@
 /**
  * С помощью свойства __proto__ задайте прототипы так,
- *  чтобы поиск любого свойства выполнялся по следующему пути: pockets → bed → table → head. 
+ *  чтобы поиск любого свойства выполнялся по следующему пути: pockets → bed → table → head.
  * Например, pockets.pen должно возвращать значение 3 (найденное в table), а bed.glasses – значение 1 (найденное в head).
  */
 
-function getHead() {
-    let head = {
+function getHead () {
+    return {
         glasses: 1
     };
-
-    return head;
 }
 
-function getTable() {
-    let table = {
+function getTable () {
+    return {
+        __proto__: getHead(),
         pen: 3,
     };
-    
-    return table;
 }
 
-function getBed() {
-    let bed = {
+function getBed () {
+    return {
+        __proto__: getTable(),
         sheet: 1,
         pillow: 2,
     };
-
-    return bed;
 }
 
-function getPockets() {
-    let pockets = {
+function getPockets () {
+    return {
+        __proto__: getBed(),
         money: 2000,
     };
-    
-
-    return pockets;
 }
 
 module.exports.getHead = getHead;
