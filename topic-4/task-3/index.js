000c902edaca9ab444a8ce5238c32ba8a95dd553
Tanya Koveshnikova <T.A.Koveshnikova@urfu.me>
2021-10-18 18:31:47 +0500

    3 переделано

--- topic-4/task-3/index.js
+++ topic-4/task-3/index.js
@@ -3,29 +3,28 @@
     Когда мы кормим одного хомяка, второй тоже наедается. Почему? Как это исправить?
  */
 
-function Hamster() {
+function Hamster () {
     this.stomach = [];
 }
 
 Hamster.prototype.eat = function(food) {
     this.stomach.push(food);
 }
-
+   
 function getSpeedy() {
     return new Hamster();
 }
-
+    
 function getLazy() {
     return new Hamster();
 }
-
-// Этот хомяк нашёл еду
-getSpeedy().eat("apple");
-console.log(getSpeedy().stomach); // apple
-
-// У этого хомяка тоже есть еда. Почему? Исправьте
-console.log(getLazy().stomach); // apple
-
-
+    
+const speedy = getSpeedy();
+speedy.eat("apple");
+console.log(speedy.stomach); 
+const lazy = getLazy();
+console.log(lazy.stomach); // apple
+    
+    
 module.exports.getSpeedy = getSpeedy;
 module.exports.getLazy = getLazy;
